2698b50300efbc278e43a665237681b705ab8437
raimondasruzgas <89936697+raimondasruzgas@users.noreply.github.com>
2021-09-03 20:57:16 +0300

    Nusikopijavau g=failus

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sarasas = new string[100];
+            for (int i = 0; i < sarasas.Length; i++)
+            {
+                Console.WriteLine(sarasas[i]);
+            }
+        }
+    }
+}
